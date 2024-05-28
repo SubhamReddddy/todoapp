9993e588ad0e62d81be7e3f5de0e9c862c3efbef
SubhamReddddy <subhamreddy121@gmail.com>
2024-05-29 00:24:30 +0530

    updating app.js file

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { config } from "dotenv";
-import path, { dirname } from "path";
+import path from "path";
 import userRouter from "./Routers/user.js";
 import taskRouter from "./Routers/task.js";
 import CookieParser from "cookie-parser";
 import cors from "cors";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// import { fileURLToPath } from "url";
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 //configer dotenv path
 config({ path: path.join(path.resolve(), "/Config/config.env") });
@@ -28,10 +28,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(CookieParser());
-app.use(express.static(path.join(__dirname, "../Simple_todo/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../Simple_todo/dist/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "../Simple_todo/dist")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../Simple_todo/dist/index.html"));
+// });
 
 //routes
 app.use("/users", userRouter);
